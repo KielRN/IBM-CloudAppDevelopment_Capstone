68850e14d9acf0411bd7ef7b9c69bdf888e062fe
ElliottRN <107939363+KielRN@users.noreply.github.com>
2024-01-03 07:30:24 -0600

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -5,15 +5,26 @@ from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
-    path('static/', views.static_view, name='static'), 
-    path('about/', views.about_view, name='about'),
-    path('contact/', views.contact_view, name='contact'),      
-    path('registration/', views.registration_request, name='registration'),
-    path('login/', views.login_request, name='login'),
-    path('logout/', views.logout_request, name='logout'),
+    # route is a string contains a URL pattern
+    # view refers to the view function
+    # name the URL
+
+    # path for about view
+    path(route='about/', view=views.about, name='about'),
+    
+    # path for contact us view
+    path(route='contact/', view=views.contact, name='contact'),
+    
+    # path for registration
+    path(route='registration/', view=views.registration_request, name='registration'),
+    # path for login
+     path(route='login/', view=views.login_request, name='login'),
+    
+    # path for logout
+    path(route='logout/', view=views.logout_request, name='logout'),
 
     path(route='', view=views.get_dealerships, name='index'),
-    #paths by ElliottRN
+
     # path for dealer reviews view
     path(route='dealer/<int:dealer_id>/', view=views.get_dealer_details, name='dealer_details'),
     
